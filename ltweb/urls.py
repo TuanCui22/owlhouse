4b8ccdf373afffcfb4253c769330a0942abda1c8
Kanye22 <124656776+TuanCui22@users.noreply.github.com>
2023-12-29 18:16:01 +0700

    Update urls.py

--- ltweb/urls.py
+++ ltweb/urls.py
@@ -4,12 +4,17 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Định nghĩa các URL cho ứng dụng
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('polls.urls')),
 ]
 
+# Nếu ở chế độ DEBUG, thêm cấu hình cho tệp tĩnh
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Nếu ở chế độ DEBUG, thêm cấu hình cho tệp đa phương tiện
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
